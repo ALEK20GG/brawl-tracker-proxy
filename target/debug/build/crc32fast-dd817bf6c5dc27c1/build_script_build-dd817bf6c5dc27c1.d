f80fc219a3db1a6a080d2e6e8d12e344576088b6
ALEK20GG <alez.lego@gmail.com>
2025-07-27 00:29:26 +0200

    build cleaning

---
+++ target/debug/build/crc32fast-dd817bf6c5dc27c1/build_script_build-dd817bf6c5dc27c1.d
@@ -0,0 +1,5 @@
+C:\Users\alezl\OneDrive\Desktop\ale\informatica\javascript\brawl-tracker-proxy\target\debug\build\crc32fast-dd817bf6c5dc27c1\build_script_build-dd817bf6c5dc27c1.d: C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
+
+C:\Users\alezl\OneDrive\Desktop\ale\informatica\javascript\brawl-tracker-proxy\target\debug\build\crc32fast-dd817bf6c5dc27c1\build_script_build-dd817bf6c5dc27c1.exe: C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
+
+C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs:
---
+++ target/debug/deps/adler2-71153da1e82f0128.d
@@ -0,0 +1,8 @@
+C:\Users\alezl\OneDrive\Desktop\ale\informatica\javascript\brawl-tracker-proxy\target\debug\deps\adler2-71153da1e82f0128.d: C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs
+
+C:\Users\alezl\OneDrive\Desktop\ale\informatica\javascript\brawl-tracker-proxy\target\debug\deps\libadler2-71153da1e82f0128.rlib: C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs
+
+C:\Users\alezl\OneDrive\Desktop\ale\informatica\javascript\brawl-tracker-proxy\target\debug\deps\libadler2-71153da1e82f0128.rmeta: C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs
+
+C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\lib.rs:
+C:\Users\alezl\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\adler2-2.0.1\src\algo.rs:
